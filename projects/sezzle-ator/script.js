40284a1a34e5c0c7466935c62f0834d5c18c8690
Mark Belain <markPuchalaii@gmail.com>
2018-12-02 16:17:15 -0600

    Updated all Calculator Functions
    It doesn't run perfectly (to my standard), but all buttons are functional and all simple functions are covered.
    
    THIS IS SEZZLE-ATOR 0.0!!

--- projects/sezzle-ator/script.js
+++ projects/sezzle-ator/script.js
@@ -1,23 +1,71 @@
-function insert(num){
- document.form.textview.value = document.form.textview.value+num;
-}
+// Just our standard "don't clog the scripts with document.getElementById()" script
+var $ = function( id ) { return document.getElementById( id ); };
 
-function equal() {
- var exp = document.form.textview.value;
- if(exp){
-  document.form.textview.value = eval(exp);
+var term1, operator, term2; // Everything is calculated by evaluating these three together.
+var flag = true; // When flag's true, next input will REPLACE the current display
+                 // Basically, it means it's time to work on the next term.
+
+
+// Insert items into the calculator's display
+function insert(num) {
+ if (flag == true && num != "%"){           // If it's time for a new term
+   flag = false;                            // Tell that to our flag
+   $("display").value = num;                // And replace the current display with out input
  }
+ else if($("display").value.includes("%")); // No more adding things if we already hit %.
+ else $("display").value += num;            // Otherwise, add our new input to the display
 }
 
-function clean(){
- document.form.textview.value = "";
+
+// A custom inster function for decimal points.
+function deci(){
+ if ($("display").value ==="0"               // Add decimal points AFTER 0's
+     || flag == true) insert("0.");          // or new terms
+ else if ($("display").value.includes(".")); // Do NOTHING if we already have one
+ else insert("."); // Otherwise, just add a decimal to the end of the current term.
 }
 
-function back(){
- var exp = document.form.textview.value;
- document.form.textview.value = exp.substring(0,exp.length-1);
+
+// This function clears the display
+function ac(){             //When you hit the AC button
+ $("display").value = "0"; //It clears the display!
+ term1 = "";
+ term2 = "";
+ operator = "";
+ flag = true;
 }
 
-function negative(){
 
+// A function explicitly called when we hit ±
+function plusminus(){
+ if ($("display").value.charAt(0) ==="-")
+    $("display").value = $("display").value.substr(1);
+ else if ($("display").value.charAt(0) ==="0"
+          && $("display").value.length == 1);
+ else $("display").value = "-" + $("display").value;
+}
+
+
+// This function is for any of the operators, ÷ × - +
+function operate (sign) {
+ term1 = percent($("display").value);
+
+ operator = sign;
+ flag = true;
+};
+
+
+// This function executes when we hit the "=" button
+function solve() {
+ term2 = percent($("display").value);
+ $("display").value = eval(term1+operator+term2);
+ flag = true;
+}
+
+
+// The function for calculating terms with a % at the end
+function percent(term){
+ if (term.includes("%"))
+  return term.substring(0, term.length-1) * 0.01;
+ else return term;
 }
